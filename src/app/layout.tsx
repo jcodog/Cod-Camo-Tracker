d7a4921234a94b0f8219cb03b4245991ee69a6ea
Jason Connolly <jcodog@jconet.co.uk>
2025-12-16 13:19:11 +0000

    chore: change shadcn layout, themes and settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Providers } from "@/components/providers";
 import { Toaster } from "@/components/ui/sonner";
 
 import "@/styles/globals.css";
+import { Nunito_Sans } from "next/font/google";
+
+const nunitoSans = Nunito_Sans({variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Cod Camo Tracker",
@@ -16,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={nunitoSans.variable}>
       <body className="min-h-screen bg-background text-foreground antialiased">
         <Providers>
           {children}
